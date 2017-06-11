d05c793e8336a722ec7da58e922f9c0b3ff5da90
Daiki-Konno <dai.kon1006@gmail.com>
2017-06-11 18:04:15 +0900

    Create warikann.js

--- 割り勘電卓/warikann.js
+++ 割り勘電卓/warikann.js
@@ -1,41 +1,40 @@
-  	(function(){
-  		'use strict';
-
-  		var priceForm = document.getElementById('price');
-  		var numForm = document.getElementById('num');
-  		var btn = document.getElementById('btn');
-  		var result = document.getElementById('result');
-
-  		priceForm.addEventListener('click',function(){
+(function(){
+　　'use strict';
+  var priceForm = document.getElementById('price');
+  var numForm = document.getElementById('num');
+  var btn = document.getElementById('btn');
+  var result = document.getElementById('result');
+  
+  priceForm.addEventListener('click',function(){
   			this.select();
   		})
-  	    numForm.addEventListener('click',function(){
+　　　　numForm.addEventListener('click',function(){
   			this.select();
   		})
 
-  		btn.addEventListener('click',function(){
-  			var price = priceForm.value;
-  			var num = numForm.value;
-  			var x1 , y1 , x2 , y2;
-  			var unit = 10;
+　　　　btn.addEventListener('click',function(){
+      var price = priceForm.value;
+  		var num = numForm.value;
+  		var x1 , y1 , x2 , y2;
+  		var unit = 10;
 
-  			if (price.match(/^[1-9][0-9]*$/) && num.match(/^[1-9][0-9]*$/)) {
-  				//ok
-  				// result.innerHTML = 'OK';
-  				if (price % num === 0) {
-  					result.innerHTML = 'ひとり'+ (price/num) +'円です。';
-  				}else{
-  					x1 = Math.floor(price / num / unit) * unit;
-  					y1 = price - (x1 * num);
-  					x2 = Math.ceil(price / num / unit) * unit;
-  					y2 = Math.abs(price - (x2 * num));
-  					result.innerHTML = 
-  					'ひとり'+x1+'円だと'+y1+'円足りません。<br>'+
-  					'ひとり'+x2+'円だと'+y2+'円余ります。'
-  				}
+  		if (price.match(/^[1-9][0-9]*$/) && num.match(/^[1-9][0-9]*$/)) {
+  			//ok
+  			// result.innerHTML = 'OK';
+  			if (price % num === 0) {
+  				result.innerHTML = 'ひとり'+ (price/num) +'円です。';
   			}else{
-  				//error
-  				result.innerHTML = '数が合いません';
-  			};
-  		})
-  	})();
+  				x1 = Math.floor(price / num / unit) * unit;
+  				y1 = price - (x1 * num);
+  				x2 = Math.ceil(price / num / unit) * unit;
+  				y2 = Math.abs(price - (x2 * num));
+  				result.innerHTML = 
+  				'ひとり'+x1+'円だと'+y1+'円足りません。<br>'+
+  				'ひとり'+x2+'円だと'+y2+'円余ります。'
+  			}
+  		}else{
+  			//error
+  			result.innerHTML = '数が合いません';
+  		};
+  	})
+  })();
